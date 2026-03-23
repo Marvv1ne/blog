4c532c65f1fdca9e52b9b6fbbbec902b9e24e720
Marvv1ne <gorn89@inbox.ru>
2026-03-23 16:06:19 +0400

    fix: add setUp method with user and use it into test_create_post method

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,15 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from .models import Post
 
 
 class ModelTest(TestCase):
-    
+    def setUp(self) -> None:
+        User = get_user_model()
+        self.user = User.objects.create()
+
     def test_create_post(self):
         title = "test title"
         slug = "test slug"
         body = "test body"
-        Post.objects.create(title=title,  slug=slug, body=body)
+        Post.objects.create(title=title, slug=slug, body=body, author=self.user)
         test_post = Post.objects.all().first()
         self.assertEqual(title, test_post.title)
         self.assertEqual(body, test_post.body)
         self.assertEqual(slug, test_post.slug)
+
